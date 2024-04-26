879b6162fe33fb36693d5a1a442e592ac97a1168
wadimba777 <vadim345plotnikov@gmail.com>
2024-04-26 15:52:06 +0300

    Init commit

---
+++ src/test/java/com/example/AircraftSpring/AircraftSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AircraftSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
